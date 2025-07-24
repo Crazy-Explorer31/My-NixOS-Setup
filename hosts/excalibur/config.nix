81abea23ee23fcbcedb52def9fd951076638bc0e
Sokolovsky Stepan <stepan.sokol@list.ru>
2025-07-24 10:48:16 +0700

    add programs

--- hosts/excalibur/config.nix
+++ hosts/excalibur/config.nix
@@ -62,7 +62,7 @@ in {
 			canTouchEfiVariables = true;
 		};
 
-		loader.timeout = 5;
+		loader.timeout = 1;
 
 		# Bootloader GRUB
 		loader.grub = {
@@ -249,6 +249,29 @@ in {
 			flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 		'';
 	};
+
+	# TLP SETUP
+	services.tlp = {
+		enable = true;
+		settings = {
+		CPU_SCALING_GOVERNOR_ON_AC = "performance";
+		CPU_SCALING_GOVERNOR_ON_BAT = "performance";
+
+		CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
+		CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+		# CPU_MIN_PERF_ON_AC = 0;
+		# CPU_MAX_PERF_ON_AC = 100;
+		# CPU_MIN_PERF_ON_BAT = 0;
+		# CPU_MAX_PERF_ON_BAT = 20;
+
+		# #Optional helps save long term battery health
+		START_CHARGE_THRESH_BAT0 = 20; # 40 and below it starts to charge
+		STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+		};
+	};
+
+	virtualisation.docker.enable = true;
 
 	# zram
 	zramSwap = {
