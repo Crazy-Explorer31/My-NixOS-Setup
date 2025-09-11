27fe49d88540c8dc86548fb34404d6901e4cbb44
Sokolovsky Stepan <stepan.sokol@list.ru>
2025-09-11 19:24:11 +0000

    change timezone

--- hosts/excalibur/config.nix
+++ hosts/excalibur/config.nix
@@ -130,7 +130,7 @@ in {
 
 	#https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 	# time.timeZone = "Asia/Krasnoyarsk"; # Set local timezone
-	time.timeZone = "Asia/Moscow"; # Set local timezone
+	time.timeZone = "Asia/Omsk"; # Set local timezone
 
 	# Select internationalisation properties.
 
